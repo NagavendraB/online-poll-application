a56acbc07b678a014e4244a5ec4dc0d31ff45a4e
Nagavendra <nagu502@gmail.com>
2021-06-09 01:58:15 +0200

    ESLint Integration + Cleanup

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import reportWebVitals from './reportWebVitals';
+import { store } from './redux/store';
 import App from './App';
 
 import './index.css';
-import { store } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
