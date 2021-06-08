4d0c6f5b04f590c459385f513252d922933f419c
Nagavendra <nagu502@gmail.com>
2021-06-09 01:24:52 +0200

    Added Question Details page

---
+++ src/pages/QuestionDetails/QuestionDetails.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledLoadingSkeletonWrapper = styled.div`
+`;
+
+export const StyledChoicesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 1px 2px 7px rgb(68 68 68 / 60%);
+`;
+
+export const StyledQuestion = styled.div`
+  margin: 38px 0 26px;
+  font-weight: bold;
+`;
+
